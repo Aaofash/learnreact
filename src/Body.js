014e0fad447c0071c058da7b2ade2d0767f02164
Aaofash <102664550+Aaofash@users.noreply.github.com>
2022-07-12 10:00:00 -0500

    reactfacts

---
+++ src/Body.js
@@ -0,0 +1,15 @@
+import React from "react"
+export default function Body() {
+    return(
+        <div className="topic">
+             <h1>Fun facts about React</h1>
+            <ul className="body-list">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walker</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>It is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    )
+}
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+export default function Header() {
+    return(
+        <header>
+            <nav className="nav">
+                <img src="https://th.bing.com/th/id/R.f81a6f373c244b1f70f4b7402b5ab372?rik=rbXh4ieLuKt%2bmA&riu=http%3a%2f%2flogos-download.com%2fwp-content%2fuploads%2f2016%2f09%2fReact_logo_logotype_emblem.png&ehk=QhGOkKcUKCU7FBQgHOajOiJqJBACUTD2Ni6LsfqzCEA%3d&risl=&pid=ImgRaw&r=0" alt="" />
+                <span className="head-name">ReactFacts</span>
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>                
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
